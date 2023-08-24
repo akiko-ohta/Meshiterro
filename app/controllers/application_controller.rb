b3a441940d9ccd6dc618ae21a71d60a00cb01176
EC2 Default User <ec2-user@ip-172-31-19-189.ap-northeast-1.compute.internal>
2023-08-24 06:42:02 +0000

    [Finish]4-11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_premitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
-    about_path
+    post_images_path
   end
-  
+
   def after_sign_out_path_for(resouece)
     about_path
   end
-  
+
   protected
-  
+
   def configure_premitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
